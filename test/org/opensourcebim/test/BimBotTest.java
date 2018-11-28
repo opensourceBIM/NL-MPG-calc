b448306a745e555cc374ba5131a52b58ec5d6896
Jasper Vijverberg <vijj@tessella.co.uk>
2018-11-28 17:47:37 +0100

    fix output to json file

--- test/org/opensourcebim/test/BimBotTest.java
+++ test/org/opensourcebim/test/BimBotTest.java
@@ -1,5 +1,6 @@
 package org.opensourcebim.test;
 
+import java.io.File;
 import java.nio.file.Path;
 
 import org.bimserver.bimbots.BimBotDummyContext;
@@ -15,6 +16,12 @@ import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 public class BimBotTest implements Runnable {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(BimBotTest.class);
@@ -23,7 +30,8 @@ public class BimBotTest implements Runnable {
 	private BimBotsServiceInterface bimBot;
 	private UsernamePasswordAuthenticationInfo credentials;
 
-	public BimBotTest(Path path, BimServerClientFactory bimServerClientFactory, UsernamePasswordAuthenticationInfo credentials, BimBotsServiceInterface bimBot) {
+	public BimBotTest(Path path, BimServerClientFactory bimServerClientFactory,
+			UsernamePasswordAuthenticationInfo credentials, BimBotsServiceInterface bimBot) {
 		this.path = path;
 		this.bimServerClientFactory = bimServerClientFactory;
 		this.credentials = credentials;
@@ -35,24 +43,37 @@ public class BimBotTest implements Runnable {
 		try (BimServerClientInterface client = bimServerClientFactory.create(credentials)) {
 			String projectName = path.getFileName().toString();
 			SProject project = client.getServiceInterface().getTopLevelProjectByName(projectName);
+
 			if (project == null) {
 				LOGGER.info("Create new project " + projectName);
 				project = client.getServiceInterface().addProject(projectName, "ifc2x3tc1");
 			} else {
 				LOGGER.info("Project " + projectName + " already existed");
 			}
+
 			if (project.getLastRevisionId() == -1) {
 				LOGGER.info("Checking-in " + projectName);
-				SDeserializerPluginConfiguration deserializer = client.getServiceInterface().getSuggestedDeserializerForExtension("ifc", project.getOid());
-				client.checkinSync(project.getOid(), "Auto checkin for BIMbot test", deserializer.getOid(), false, path);
+				SDeserializerPluginConfiguration deserializer = client.getServiceInterface()
+						.getSuggestedDeserializerForExtension("ifc", project.getOid());
+				client.checkinSync(project.getOid(), "Auto checkin for BIMbot test", deserializer.getOid(), false,
+						path);
 				project = client.getServiceInterface().getTopLevelProjectByName(projectName);
 			} else {
 				LOGGER.info("Revision already available");
 			}
+
 			try (IfcModelInterface model = client.getModel(project, project.getLastRevisionId(), true, false, true)) {
 				BimBotsInput input = new BimBotsInput("", null);
 				input.setIfcModel(model);
 				BimBotsOutput bimBotsOutput = bimBot.runBimBot(input, new BimBotDummyContext(), null);
+
+				ObjectMapper mapper = new ObjectMapper();
+				mapper.enable(SerializationFeature.INDENT_OUTPUT);
+				DefaultPrettyPrinter printer = new DefaultPrettyPrinter();
+				ObjectWriter writer = mapper.writer(printer);
+				JsonNode jsonTree = mapper.readTree(bimBotsOutput.getData());
+				writer.writeValue(new File(path.toString().replace(".ifc", ".json")), jsonTree);
+
 				LOGGER.info(bimBotsOutput.getTitle() + " " + bimBotsOutput.getData().length + "bytes");
 				LOGGER.info("");
 			}
