fe3ef8fd4c218ba41848f40c7179f441dc60736b
Jasper Vijverberg <jasper.vijverberg@tessella.com>
2019-01-25 09:27:27 +0100

    start testing NMD connection

--- src/org/opensourcebim/nmd/BimMaterialDatabaseSession.java
+++ src/org/opensourcebim/nmd/BimMaterialDatabaseSession.java
@@ -1,5 +1,8 @@
 package org.opensourcebim.nmd;
 
+import java.util.Date;
+import java.util.List;
+
 import org.opensourcebim.ifccollection.MpgMaterial;
 
 
@@ -10,20 +13,45 @@ import org.opensourcebim.ifccollection.MpgMaterial;
  */
 public class BimMaterialDatabaseSession implements EditableDataService {
 
+	private Date requestDate;
+	
 	@Override
-	public void start() {
+	public void login() {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void stop() {
+	public void logout() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public void setRequestDate(Date newDate) {
+		this.requestDate = newDate;
 	}
 
 	@Override
 	public NmdProductCard retrieveMaterial(MpgMaterial material) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<NmdProductCard> getAllProductSets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MaterialSpecification> getSpecsForProducts(List<NmdProductCard> products) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<NmdBasisProfiel> getPhaseProfiles(List<Integer> ids) {
 		// TODO Auto-generated method stub
 		return null;
 	}
