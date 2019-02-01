38e7f7b4f91401505c4afd6ba587b78be9ba86dd
Jasper Vijverberg <vijj@tessella.co.uk>
2019-02-01 15:56:45 +0100

    update replacement calculations for v3.0 to differentiate between totaal and deel producten

--- src/org/opensourcebim/mpgcalculation/MpgCalculator.java
+++ src/org/opensourcebim/mpgcalculation/MpgCalculator.java
@@ -18,9 +18,13 @@ public class MpgCalculator {
 	private MpgCalculationResults results;
 
 	public MpgCalculator() {
-		setResults(new MpgCalculationResults());
+		reset();
 	}
 
+	public void reset() {
+		setResults(new MpgCalculationResults());
+	}
+	
 	public MpgCalculationResults calculate(double designLife) {
 
 		if (objectStore == null) {
@@ -42,19 +46,20 @@ public class MpgCalculator {
 				
 				NmdProductCard product = element.getNmdProductCard();
 
+				// Determine replacements required.
+				// this is usually 1 for regular materials and > 1 for cyclic maintenance
+				// For a product card with composed profielsets (not a single totaalproduct) the replacement of the 
+				// first encountered Construction (Cuas code 1) profielset will be used.
+				double replacements = this.calculateReplacements(designLife, product);
+				
 				for (NmdProfileSet profielSet : product.getProfileSets()) {
 					
 					// get number of product units based on geometry of ifcproduct and unit of productcard
+					// TODO: currently there is a very basic method implemented. should be improved
 					double unitsRequired = profielSet.getRequiredNumberOfUnits(element.getMpgObject());
 					
 					// category 3 data requires a 30% penalty
 					double categoryMultiplier = profielSet.getCategory() == 3 ? 1.3 : 1.0;
-					
-					// Determine replacements required.
-					// this is usually 1 for regular materials and > 1 for cyclic maintenance
-					// materials. ToDo: check whether these calculations are still correct
-					double replacements = this.calculateReplacements(designLife, profielSet.getProductLifeTime(),
-							profielSet.getIsMaintenanceSpec());
 
 					// calculate total mass taking into account construction losses and replacements
 					// ToDo: how does the current approach tackle issues with plate materials etc.?
@@ -95,12 +100,19 @@ public class MpgCalculator {
 	 * @param productLife design life of the material in years
 	 * @return number of replacements. number is alsways larger or equal to 1
 	 */
-	private double calculateReplacements(double designLife, double productLife, boolean isMaintenanceMaterial) {
-		if (isMaintenanceMaterial) {
-			return Math.max(2.0, designLife / Math.max(1.0, productLife) - 1.0) - 1.0;
-		} else {
-			return Math.max(1.0, designLife / Math.max(1.0, productLife));
+	private double calculateReplacements(double designLife, NmdProductCard product) {
+		double productLife = -1.0;
+		if (product.getIsTotaalProduct()) {
+			productLife = (double)product.getProfileSets().stream()
+					.findFirst().get().getProductLifeTime();
+		} else
+		{
+			productLife = (double)product.getProfileSets().stream()
+					.filter(p -> p.getCuasCode() == 1)
+					.findFirst().get().getProductLifeTime();
 		}
+
+		return Math.max(1.0, designLife / Math.max(1.0, productLife));
 	}
 
 	public MpgCalculationResults getResults() {
