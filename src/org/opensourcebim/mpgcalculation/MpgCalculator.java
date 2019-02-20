743b0e620222ebcab615ed39c91c077d18306bbd
Jasper Vijverberg <vijj@tessella.co.uk>
2019-02-20 18:02:09 +0100

    move unit conversion and dimension checks to static methods

--- src/org/opensourcebim/mpgcalculation/MpgCalculator.java
+++ src/org/opensourcebim/mpgcalculation/MpgCalculator.java
@@ -6,9 +6,11 @@ import org.bimserver.utils.AreaUnit;
 import org.bimserver.utils.LengthUnit;
 import org.opensourcebim.ifccollection.MpgElement;
 import org.opensourcebim.ifccollection.MpgObjectStore;
+import org.opensourcebim.ifccollection.MpgScalingOrientation;
 import org.opensourcebim.nmd.NmdProductCard;
 import org.opensourcebim.nmd.NmdProfileSet;
 import org.opensourcebim.nmd.scaling.NmdScaler;
+import org.opensourcebim.nmd.scaling.NmdScalingUnitConverter;
 
 /**
  * Do the MPG calculations based on a read in object model. with material data
@@ -21,6 +23,7 @@ public class MpgCalculator {
 
 	private MpgObjectStore objectStore = null;
 	private MpgCalculationResults results;
+	private NmdScalingUnitConverter converter;
 
 	public MpgCalculator() {
 		reset();
@@ -47,7 +50,7 @@ public class MpgCalculator {
 
 			// for each building material found:
 			for (MpgElement element : objectStore.getElements()) {
-				
+
 				for (NmdProductCard product : element.getNmdProductCards()) {
 
 					// Determine replacements required.
@@ -56,7 +59,7 @@ public class MpgCalculator {
 					// replacement of the
 					// first encountered Construction (Cuas code 1) profielset will be used.
 					double replacements = this.calculateReplacements(designLife, product);
-					
+
 					// category 3 data requires a 30% penalty
 					double categoryMultiplier = product.getCategory() == 3 ? 1.3 : 1.0;
 
@@ -65,7 +68,7 @@ public class MpgCalculator {
 					// TODO: currently there is a very basic method implemented. should be improved.
 					// This should include density conversions and/or energy water use conversion
 					double unitsRequired = product.getRequiredNumberOfUnits(element.getMpgObject());
-					
+
 					for (NmdProfileSet profielSet : product.getProfileSets()) {
 						double scaleFactor = 1.0;
 						// determine scale factor based on scaler. if no scaler is present the
@@ -74,21 +77,22 @@ public class MpgCalculator {
 
 							NmdScaler scaler = profielSet.getScaler();
 							String unit = scaler.getUnit();
-							int numDims = getUnitDimension(product.getUnit());
+							int numDims = NmdScalingUnitConverter.getUnitDimension(product.getUnit());
 							if (numDims < 3) {
-								
-								Double unitConversionFactor = getScalingUnitConversionFactor(unit, dims.length);
-								
+
 								MpgScalingOrientation or = element.getMpgObject().getGeometry().getScalerOrientation(numDims);
 								Double[] dims = or.getScaleDims();
+								Double unitConversionFactor = NmdScalingUnitConverter
+										.getScalingUnitConversionFactor(unit, dims.length, this.getObjectStore());
+
 								scaleFactor = profielSet.getScaler().scaleWithConversion(dims, unitConversionFactor);
 							}
 						}
 
 						// calculate total units required taking into account category modifiers.
 						// replacements, # of profielSet items per productCard and scaling
-						double lifeTimeUnitsPerProfiel = replacements * profielSet.getQuantity()
-								* unitsRequired * categoryMultiplier * scaleFactor;
+						double lifeTimeUnitsPerProfiel = replacements * profielSet.getQuantity() * unitsRequired
+								* categoryMultiplier * scaleFactor;
 
 						// example for production
 						profielSet.getAllFaseProfielen().values().forEach(fp -> {
@@ -108,48 +112,6 @@ public class MpgCalculator {
 		return results;
 	}
 
-	/**
-	 * Converts the This might be slightly counter intuititve, but when scaling over
-	 * a single dimension we need a 2D conversionfactor while when we are scaling
-	 * over 2 dimensions the scaling is done per axis and therefore the conversion
-	 * is 1 D first figure out the quantity of the input unit and get the right
-	 * store unit
-	 * 
-	 * @param unit working unit of the nmd scaler
-	 * @param dims dimensionality of the object scaling dimensions (1 or 2)
-	 * @return conversion factor to convert from scaling unit mpgObject geometry
-	 *         unit.
-	 */
-	private Double getScalingUnitConversionFactor(String unit, int dims) {
-		Double factor = 1.0;
-		if (dims == 2) {
-			switch (unit.toLowerCase()) {
-			case "mm":
-			case "millimiter":
-				factor = this.objectStore.getLengthUnit().convert(1.0, LengthUnit.MILLI_METER);
-				break;
-			case "m":
-			case "meter":
-				factor = this.objectStore.getLengthUnit().convert(1.0, LengthUnit.METER);
-			default:
-				break;
-			}
-		} else if (dims == 1) {
-			switch (unit.toLowerCase()) {
-			case "mm":
-			case "millimiter":
-				factor = this.objectStore.getAreaUnit().convert(1.0, AreaUnit.SQUARED_MILLI_METER);
-				break;
-			case "m":
-			case "meter":
-				factor = this.objectStore.getAreaUnit().convert(1.0, AreaUnit.SQUARED_METER);
-			default:
-				break;
-			}
-		}
-		return factor;
-	}
-
 	public MpgObjectStore getObjectStore() {
 		return objectStore;
 	}
@@ -164,7 +126,7 @@ public class MpgCalculator {
 	 * https://www.milieudatabase.nl/imgcms/20141125_SBK_BepMeth_vs_2_0_inclusief_Wijzigingsblad_1_juni_2017_&_1_augustus_2017.pdf
 	 * 
 	 * @param designLife total duration that building should be usable in years
-	 * @param element element with products linked to it
+	 * @param card     productcard with product lifetime
 	 * @return number of replacements. number is alsways larger or equal to 1
 	 */
 	private Double calculateReplacements(double designLife, NmdProductCard card) {
@@ -181,36 +143,4 @@ public class MpgCalculator {
 	public void setResults(MpgCalculationResults results) {
 		this.results = results;
 	}
-	
-	private int getUnitDimension(String unit) {	
-		switch (unit.toLowerCase()) {
-		case "mm":
-		case "cm":
-		case "m1":
-		case "m":
-		case "meter":
-			return 1;
-		case "mm2":
-		case "mm^2":
-		case "square_millimeter":
-		case "cm2":
-		case "cm^2":
-		case "m2":
-		case "m^2":
-		case "square_meter":
-			return 2;
-		case "mm3":
-		case "mm^3":
-		case "cubic_millimeter":
-		case "cm3":
-		case "cm^3":
-		case "m3":
-		case "m^3":
-		case "cubic_meter":
-			return 3;
-		default:
-			return -1;
-		}
-	}
-
 }
---
+++ src/org/opensourcebim/nmd/scaling/NmdScalingUnitConverter.java
@@ -0,0 +1,89 @@
+package org.opensourcebim.nmd.scaling;
+
+import org.bimserver.utils.AreaUnit;
+import org.bimserver.utils.LengthUnit;
+import org.opensourcebim.ifccollection.MpgObjectStore;
+
+/**
+ * Static functions to support dimension chekcs and unit conversion
+ * @author vijj
+ *
+ */
+public final class NmdScalingUnitConverter {
+
+	private NmdScalingUnitConverter() {	}
+	
+	public static int getUnitDimension(String unit) {	
+		switch (unit.toLowerCase()) {
+		case "mm":
+		case "cm":
+		case "m1":
+		case "m":
+		case "meter":
+			return 1;
+		case "mm2":
+		case "mm^2":
+		case "square_millimeter":
+		case "cm2":
+		case "cm^2":
+		case "m2":
+		case "m^2":
+		case "square_meter":
+			return 2;
+		case "mm3":
+		case "mm^3":
+		case "cubic_millimeter":
+		case "cm3":
+		case "cm^3":
+		case "m3":
+		case "m^3":
+		case "cubic_meter":
+			return 3;
+		default:
+			return -1;
+		}
+	}
+	
+	/**
+	 * Converts the This might be slightly counter intuititve, but when scaling over
+	 * a single dimension we need a 2D conversionfactor while when we are scaling
+	 * over 2 dimensions the scaling is done per axis and therefore the conversion
+	 * is 1 D first figure out the quantity of the input unit and get the right
+	 * store unit
+	 * 
+	 * @param unit working unit of the nmd scaler
+	 * @param dims dimensionality of the object scaling dimensions (1 or 2)
+	 * @return conversion factor to convert from scaling unit mpgObject geometry
+	 *         unit.
+	 */
+	public static Double getScalingUnitConversionFactor(String unit, int dims, MpgObjectStore store) {
+		Double factor = 1.0;
+		if (dims == 2) {
+			switch (unit.toLowerCase()) {
+			case "mm":
+			case "millimiter":
+				factor = store.getLengthUnit().convert(1.0, LengthUnit.MILLI_METER);
+				break;
+			case "m":
+			case "meter":
+				factor = store.getLengthUnit().convert(1.0, LengthUnit.METER);
+			default:
+				break;
+			}
+		} else if (dims == 1) {
+			switch (unit.toLowerCase()) {
+			case "mm":
+			case "millimiter":
+				factor = store.getAreaUnit().convert(1.0, AreaUnit.SQUARED_MILLI_METER);
+				break;
+			case "m":
+			case "meter":
+				factor = store.getAreaUnit().convert(1.0, AreaUnit.SQUARED_METER);
+			default:
+				break;
+			}
+		}
+		return factor;
+	}
+	
+}
