5ac4a7881c3218a2697702f8c8db68819e1a55cf
Jasper Vijverberg <vijj@tessella.co.uk>
2019-08-27 16:16:00 +0200

    use nlsfb codes from parent when no viable own nlsfb code is present

--- src/org/opensourcebim/mapping/NmdDataResolverImpl.java
+++ src/org/opensourcebim/mapping/NmdDataResolverImpl.java
@@ -639,6 +639,15 @@ public class NmdDataResolverImpl implements NmdDataResolver {
 
 			if (foundMap != null && foundMap.size() > 0) {
 				o.addNlsfbAlternatives(new HashSet<String>(foundMap));
+			} else {
+				// get type of possible parent and get nlsfb alternatives from there
+				Optional<MpgObject> parentObj = this.getStore().getObjectByGuid(o.getParentId());
+				if (parentObj.isPresent()) {
+					foundMap = map.getOrDefault(parentObj.get().getObjectType(), null);
+					if (foundMap != null && foundMap.size() > 0) {
+						o.addNlsfbAlternatives(new HashSet<String>(foundMap));
+					}
+				}
 			}
 		});
 	}
